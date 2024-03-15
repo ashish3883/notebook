7bd37c5e81877bf3f4df2278d2568d260b344c2b
Ashish Kumar <ashishkumar3883@gmail.com>
2024-03-15 16:59:36 +0530

    Update 15-3-24

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,88 +10,89 @@ const fetchuser = require('../middleware/fetchuser');
 const JWT_Secret = 'Ashishisagoodb#oy'
 
 //Route 1: Create a user using: POST "/api/auth/createuser". No login required.
-router.post("/createuser",[
-    body('email', "Enter valid email").isEmail(),
-    body('name', "Enter valid name").isLength({ min: 3 }),
-    body('password', "Enter valid password").isLength({ min: 5 }),
-], async (req, res)=>{
-    //If there are errors, request bad request & errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+router.post("/createuser", [
+  body('email', "Enter valid email").isEmail(),
+  body('name', "Enter valid name").isLength({ min: 3 }),
+  body('password', "Enter valid password").isLength({ min: 5 }),
+], async (req, res) => {
+  let success = false;
+  //If there are errors, request bad request & errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ success, errors: errors.array() });
+  }
+  //Check whether the user with this email exist already
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ success, error: "Sorry user with same email already exist" })
     }
-    //Check whether the user with this email exist already
-    try {      
-      let user = await User.findOne({email: req.body.email});
-      if(user){
-        return res.status(400).json({error:"Sorry user with same email already exist"})
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt)
+    //Create User
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: secPass,
+    })
+    const data = {
+      user: {
+        id: user.id
       }
-      const salt = await bcrypt.genSalt(10);
-      const secPass = await bcrypt.hash(req.body.password, salt)
-      //Create User
-      user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: secPass,
-      })
-      const data = {
-        user:{
-          id: user.id
-        }
-      }
+    }
 
-      const authToken = jwt.sign(data, JWT_Sercret);
-      
-      res.send({authToken})
-    } catch (error) {
-      console.log(error.message)
-      res.status(500).send("Some error occured")
-    }
-      // .then(user => res.json(user))
-      // .catch(err=>{console.log(err) 
-      //   res.json({error:"Please enter unique value for email", message:err.message})})
+    const authToken = jwt.sign(data, JWT_Secret);
+    success = true;
+    res.send({ success, authToken })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send("Some error occured")
+  }
+  // .then(user => res.json(user))
+  // .catch(err=>{console.log(err) 
+  //   res.json({error:"Please enter unique value for email", message:err.message})})
 })
 
 //Route 2: Authenticate a user using: POST "/api/auth/login". No login required.
-router.post("/login",[
-    body('email', "Enter valid email").isEmail(),
-    body('password', "Password cannot be blank").exists(),
-], async (req, res)=>{
-    let success = false;
-    //If there are errors, request bad request & errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ success, errors: errors.array() });
+router.post("/login", [
+  body('email', "Enter valid email").isEmail(),
+  body('password', "Password cannot be blank").exists(),
+], async (req, res) => {
+  let success = false;
+  //If there are errors, request bad request & errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ success, errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ success, error: "Please enter correct creds" })
     }
+    const passwordCompare = await bcrypt.compare(password, user.password);
 
-    const {email, password} = req.body;
-
-    try {
-      let user = await User.findOne({email});
-      if(!user){
-        return res.status(400).json({success, error:"Please enter correct creds"})
-      }
-      const passwordCompare = await bcrypt.compare(password, user.password);
-
-      if(!passwordCompare){
-        return res.status(400).json({success, error:"Please enter correct creds"})
-      }
-      const data = {
-        user:{
-          id: user.id
-        }
-      }     
-      const authToken = jwt.sign(data, JWT_Secret);
-      success = true;
-      res.send({success,authToken})
-    } catch (error) {
-      console.log(error.message)
-      res.status(500).send("Some error occured")
+    if (!passwordCompare) {
+      return res.status(400).json({ success, error: "Please enter correct creds" })
     }
-  })
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authToken = jwt.sign(data, JWT_Secret);
+    success = true;
+    res.send({ success, authToken })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send("Some error occured")
+  }
+})
 
 //Route 3: Get user details using: POST "/api/auth/getuser". Login required.
-router.post("/getuser", fetchuser, async (req, res)=>{
+router.post("/getuser", fetchuser, async (req, res) => {
   try {
     const userId = req.user.id;
     const user = await User.findById(userId).select('-password');
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+  const [cred, setCred] = useState({name:"", email:"", password:"", confirmpassword:""});
+  let navigate = useNavigate();
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const {name, email, password, confirmpassword} = cred;
+    if(password!==confirmpassword){
+      alert("Password & Confirm password didn't same");
+      return;
+    }
+    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({name, email, password}),
+      });
+      const json = await response.json();
+      console.log(json)
+      if(json.success){
+        //Save token & redirect
+        localStorage.setItem('token', json.authtoken);
+        navigate("/")
+      }
+      else{
+        alert("Enter correct creds")
+      }
+}
+const handleChange=(e)=>{
+    setCred({...cred, [e.target.name]:e.target.value})
+}
   return (
-    <div>
-        I am Signup
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name*</label>
+          <input type="text" className="form-control" id="name" name='name' onChange={handleChange} aria-describedby="emailHelp" required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email*</label>
+          <input type="email" className="form-control" id="email" name='email' onChange={handleChange} aria-describedby="emailHelp" required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password*</label>
+          <input type="password" className="form-control" id="password" name='password' onChange={handleChange} minLength={5} required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmpassword" className="form-label">Confirm Password*</label>
+          <input type="password" className="form-control" id="confirmpassword" name='confirmpassword' onChange={handleChange} minLength={5} required/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   )
 }
